1fb67d73ef9172ca264212d991008eb97d936336
Ryan Ballantine <rgballantine@gmail.com>
2021-07-15 23:20:45 +0200

    Bootstrap icons patch

--- src/components/BookTable.js
+++ src/components/BookTable.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import './BookTable.css';
 
 export default class BookTable extends Component {
     render() {
@@ -16,11 +18,11 @@ export default class BookTable extends Component {
                         {
                             this.props.books.map(book => {
                                 return <tr key={book.id}>
-                                    <th>{book.name}</th>
+                                    <td>{book.name}</td>
                                     <td>{book.author}</td>
                                     <td>{book.isbn}</td>
                                     <td>
-                                        <i className="bi bi-trash pointer" onClick={() => this.props.bookRemoved(book.id)}>delete</i>
+                                        <i className="bi bi-trash pointer" onClick={() => this.props.bookRemoved(book.id)}></i>
                                     </td>
                                 </tr>
                             })
